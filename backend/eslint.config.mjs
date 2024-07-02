fad843c917c1825577e9127509abf16dfeec8bb6
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-07-02 15:08:19 +0300

    configuring eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,8 +3,30 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
+  "eslint:recommended",
+  "plugin:@typescript-eslint/recommended",
+  "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  "prettier",
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    rules: {
+    "@typescript-eslint/semi": ["error"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { "argsIgnorePattern": "^_" }
+    ],
+    "no-case-declarations": "off"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+      "project": "./tsconfig.json"
+    }
+  },
+  { languageOptions: { globals: globals.browser } },
 ];
---
+++ timekeeping.md
@@ -0,0 +1,4 @@
+
+| Date | Hours | What I did |
+| ---- | ----- | ---------- |
+| 02.07.2024 | 1 | Started initializing project backend and config |
