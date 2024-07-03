2c264b0c1c79f38cfe5318f32b388b37addb6002
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-07-03 11:13:56 +0300

    updated package.json and eslint config to a working state

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,32 +1,10 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default [
-  pluginJs.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  "eslint:recommended",
-  "plugin:@typescript-eslint/recommended",
-  "plugin:@typescript-eslint/recommended-requiring-type-checking",
-  "prettier",
-  {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    rules: {
-    "@typescript-eslint/semi": ["error"],
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/restrict-template-expressions": "off",
-    "@typescript-eslint/restrict-plus-operands": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { "argsIgnorePattern": "^_" }
-    ],
-    "no-case-declarations": "off"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-      "project": "./tsconfig.json"
-    }
-  },
-  { languageOptions: { globals: globals.browser } },
-];
+  ...tseslint.configs.strict,
+  eslintConfigPrettier
+);
