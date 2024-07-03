87aad2e602c30439a2372a76ef2df4d283db4a12
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-07-03 12:50:27 +0300

    added dummy data and images endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import path from 'path'
 const app = express();
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/images', express.static(path.join(__dirname, 'data/images')));
 
 const PORT = 3003;
 
