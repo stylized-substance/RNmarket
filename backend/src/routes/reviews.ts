0f14288c5a9fe95eec9aa142d7d5d9354d425c5d
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-08-21 14:03:51 +0300

    updated reviews router

--- backend/src/routes/reviews.ts
+++ backend/src/routes/reviews.ts
@@ -5,26 +5,46 @@ import { parseString } from '#src/utils/typeNarrowers';
 
 const router: Router = Router();
 
-// Get reviews for user based on database primary key
+// Get reviews
 router.get('/', async (req: Request, res: Response) => {
-  const user_id: string = parseString(req.query.user_id);
+  if (Object.keys(req.query).length === 0) {
+    return res.status(400).json('Query parameter missing')
+  }
+
+  // Get reviews for user
+  const user_id: string | undefined = req.query.user_id
+    ? parseString(req.query.user_id)
+    : undefined;
+  const product_id: string | undefined = req.query.product_id
+    ? parseString(req.query.product_id)
+    : undefined;
+  let reviews: ReviewModel[] | [] = [];
 
   if (user_id) {
-    const reviews: ReviewModel[] | [] = await ReviewModel.findAll({
+    reviews = await ReviewModel.findAll({
       where: {
         user_id: {
-          [Op.eq]: req.query.user_id
+          [Op.eq]: user_id
         }
       }
     });
+  }
 
-    if (reviews && reviews.length > 0) {
-      res.json(reviews);
-    } else {
-      res.status(404).json('No reviews found');
-    }
+  // Get reviews for product
+  if (product_id) {
+    reviews = await ReviewModel.findAll({
+      where: {
+        product_id: {
+          [Op.eq]: product_id
+        }
+      }
+    });
+  }
+
+  if (reviews && reviews.length > 0) {
+    return res.json(reviews);
   } else {
-    res.status(400).json('user_id query parameter missing');
+    return res.status(404).json('No reviews found');
   }
 });
 
