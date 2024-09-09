82d9dc52a1e932706d73a60283c70af2e9beb754
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-09-09 14:54:53 +0300

    started writing tests for products router

--- backend/__tests__/products.test.ts
+++ backend/__tests__/products.test.ts
@@ -5,8 +5,6 @@ import { connectToDatabase, dropAllTables } from '#src/utils/database';
 
 const api = supertest(app);
 
-console.log('node_env', process.env.NODE_ENV)
-
 beforeAll(async () => {
   await dropAllTables();
   await connectToDatabase();
@@ -16,4 +14,5 @@ test('GET /api/products', async () => {
   const response = await api.get('/api/products');
   expect(response.status).toBe(200);
   expect(response.headers['content-type']).toMatch(/application\/json/)
+  expect(response.body.products.length).toEqual(50)
 });
