28f797fd402701c6f4f8bc2457bd3c40d7689d9f
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-09-10 10:06:44 +0300

    added custom assert function

--- backend/__tests__/products.test.ts
+++ backend/__tests__/products.test.ts
@@ -1,22 +1,36 @@
-// import { beforeEach } from 'node:test';
 import supertest from 'supertest';
 import app from '#src/app';
 import { connectToDatabase, closeDatabaseConnection, dropAllTables } from '#src/utils/database';
+import Response from 'superagent/lib/node/response'
 
 const api = supertest(app);
 
 beforeEach(async () => {
+  // Empty database and run migrations
   await dropAllTables();
   await connectToDatabase();
 });
 
 describe('GET requests', () => {
-  test('/api/products returns all products from database', async () => {
-    const response = await api.get('/api/products');
+  // Custom assert function to reduce boilerplate
+  const assertGetResponse = (response: Response) => {
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toMatch(/application\/json/)
+  }
+  
+  test('GET /api/products returns all products from database', async () => {
+    const response = await api.get('/api/products');
+    assertGetResponse(response);
     expect(response.body.products.length).toEqual(50)
   });
+
+  describe('With query parameters', () => {
+    test('limit=1 returns one product', async () => {
+      const response = await api.get('/api/products').query('limit=1');
+      assertGetResponse(response);
+      expect(response.body.products.length).toEqual(1)
+    })
+  })
 })
 
 afterAll(async () => {
