dc18cda97fb3b74cd8f757755b83521308c6d5b6
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-09-14 15:54:59 +0300

    added test for getting reviews with product_id query

--- backend/__tests__/reviews.test.ts
+++ backend/__tests__/reviews.test.ts
@@ -10,7 +10,7 @@ import {
   assertValidReview
 } from '#src/utils/testHelpers';
 import { Review as ReviewModel } from '#src/models';
-import { User as UserModel } from '#src/models';
+import { User as UserModel, Product as ProductModel } from '#src/models';
 
 const api = supertest(app);
 
@@ -21,7 +21,7 @@ beforeAll(async () => {
 });
 
 describe('GET requests', () => {
-  test.only('Request with query parameter user_id returns reviews for single user', async () => {
+  test('Request with query parameter user_id returns reviews for single user', async () => {
     // Get user that has at least one review
     const user: UserModel | null = await UserModel.findOne({
       include: [
@@ -40,6 +40,26 @@ describe('GET requests', () => {
     response.body.reviews.forEach((review: unknown) =>
       assertValidReview(review)
     );
+  });
+  test('Request with query parameter product_id returns reviews for single product', async () => {
+    // Get product that has at least one review
+    const product: ProductModel | null = await ProductModel.findOne({
+      include: {
+        model: ReviewModel,
+        required: true
+      }
+    });
+
+    if (product) {
+      const response = await api
+        .get('/api/reviews')
+        .query(`product_id=${product.dataValues.id}`);
+      assert200GetResponse(response);
+      expect(response.body).toHaveProperty('reviews');
+      response.body.reviews.forEach((review: unknown) =>
+        assertValidReview(review)
+      );
+    }
   });
 });
 
