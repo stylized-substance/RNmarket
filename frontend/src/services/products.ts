156e8567b37f638db45b14afb1c072aa1813f09f
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-09-25 14:40:04 +0300

    working on productsService

--- frontend/src/services/products.ts
+++ frontend/src/services/products.ts
@@ -1,12 +1,18 @@
 import axios from 'axios';
-
-import { Product } from '../types/types';
+// import { Product } from '../types/types';
+// import { toProduct } from '#src/utils/typeNarrowers';
 
 const apiUrl = 'http://localhost:3003';
 
 const getAll = async () => {
-  const products = await axios.get<Product[]>(`${apiUrl}/products`);
-  return products;
+  const response = await axios.get(`${apiUrl}/products`);
+  console.log(response)
+  // let products: unknown = await axios.get(`${apiUrl}/products`);
+  // if (Array.isArray(products)) {
+  //   products = products.map(product => toProduct(product))
+  //   products
+  // }
+  // return products;
 };
 
 export default { getAll };
--- frontend/src/utils/typeNarrowers.ts
+++ frontend/src/utils/typeNarrowers.ts
@@ -6,8 +6,40 @@ import {
   ClothingItem,
   BeautyItem,
   FurnitureItem,
-  Laptop
+  Laptop,
+  ProductCategory
 } from '#src/types/types';
+
+const isString = (param: unknown): param is string => {
+  return typeof param === 'string';
+};
+
+const isNumber = (param: unknown): param is number => {
+  let value;
+
+  if (typeof param === 'string') {
+    value = Number(param);
+  } else {
+    value = param;
+  }
+
+  return typeof value === 'number' && !isNaN(value);
+};
+
+const isObject = (param: unknown): param is object => {
+  return param !== null && typeof param === 'object';
+};
+
+const isStringArray = (param: unknown): param is string[] => {
+  return Array.isArray(param) && param.every((item) => isString(item));
+};
+
+const isProductCategory = (param: string): param is ProductCategory => {
+  console.log(typeof param)
+  return Object.values(ProductCategory)
+    .map((value) => value.toString())
+    .includes(param);
+};
 
 const isProduct = (param: unknown): param is Product => {
   return (
@@ -140,3 +172,5 @@ const toProduct = (param: unknown): Product => {
     }
   }
 };
+
+export { toProduct }
