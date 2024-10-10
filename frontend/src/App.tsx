63113775df6779ed1bc33627156c5dcb24fbc71c
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-10-10 16:22:18 +0300

    working on frontpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import './styles/custom.css'
 import { useState, useEffect } from 'react';
 import { Product } from './types/types';
 import productsService from './services/products';
@@ -10,6 +11,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const App = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -46,19 +48,20 @@ const App = () => {
   });
 
   return (
-    <Container>
-      <Navbar>
-        <Container>
-          <Navbar.Brand>RNmarket</Navbar.Brand>
-          <Navbar.Collapse>
-            <Nav>
-              <Nav.Link>Home</Nav.Link>
-            </Nav>
-            <Form className='w-100'>
-              <Form.Control type="search" placeholder="Search" />
-            </Form>
-          </Navbar.Collapse>
-        </Container>
+    <Container style={{ marginTop: "60px" }}>
+      <Navbar fixed="top" expand="sm" className="navbar">
+        <Navbar.Brand>RNmarket</Navbar.Brand>
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link style={{ color:"white" }}>Home</Nav.Link>
+          </Nav>
+          <Form className="w-100 ms-2 me-2">
+            <Form.Control type="search" placeholder="Search" />
+          </Form>
+          <Button variant="light">
+            Login
+          </Button>
+        </Navbar.Collapse>
       </Navbar>
       <h1>All products</h1>
       <Row lg={6}>
