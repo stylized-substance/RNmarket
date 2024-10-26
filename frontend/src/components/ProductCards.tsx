88ac2e875a3e46b6dca2279e217ce02b9c071d15
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-10-26 11:29:53 +0300

    extracted product cards to separate component

---
+++ frontend/src/components/ProductCards.tsx
@@ -0,0 +1,47 @@
+import { Product } from '#src/types/types'
+
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+interface ProductCardsProps {
+  products: Product[]
+}
+
+
+const ProductCards = (props: ProductCardsProps) => {
+  console.log('products', props)
+
+  const productsToCards = props.products.map((product) => {
+    let imageUrl = '';
+    if (product.imgs !== null && product.imgs !== undefined) {
+      imageUrl = `http://localhost:3003${product.imgs[0]}`;
+    }
+  
+    return (
+      <Card key={product.id}>
+        <Card.Img
+          variant="top"
+          src={imageUrl}
+          style={{ height: 200, width: 'auto' }}
+        />
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Subtitle>{product.price}</Card.Subtitle>
+          <Card.Text>{product.specs[0].substring(0, 20)}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  });
+  
+  return (
+    <Row lg={6}>
+      {productsToCards.map((card) => (
+        <Col key={card.key}>{card}</Col>
+      ))}
+    </Row>
+  );
+}
+
+export default ProductCards
+
