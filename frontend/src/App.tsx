5ca45c7d1af5840fa965e65f54c27f7e9e9e43df
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-10-31 10:02:32 +0200

    Revert "Started implementing React Context and building single product page"
    This reverts commit ce177abefbe6a6ffe96db986940fee8401eefd68.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,28 +3,15 @@ import '#src/styles/custom.css';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { useContext } from 'react';
-import ProductContext from '#src/context/ProductContext.tsx';
-
-
 import Home from '#src/components/Home';
 import NavBar from '#src/components/NavBar';
 import Products from '#src/components/Products';
-import SingleProduct from '#src/components/SingleProduct';
-import { ProductContextProvider } from '#src/context/ProductContext';
 
 import Container from 'react-bootstrap/Container';
 
 const App = () => {
   const [adminLoggedIn, setAdminLoggedIn] = useState<boolean>(true);
   const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
-
-  const context = useContext(ProductContext)
-  console.log('context', context)
-  if (context) {
-    // const id = context.id
-
-  }
 
   return (
     <BrowserRouter>
@@ -36,33 +23,24 @@ const App = () => {
           setUserLoggedIn={setUserLoggedIn}
         />
       </Container>
-      <ProductContextProvider>
-        <Routes>
-          <Route path="/" element=<Home /> />
-          <Route
-            path="/products/mobiles"
-            element=<Products productCategory="Mobiles" />
-          />
-          <Route
-            path="/products/furniture"
-            element=<Products productCategory="Furniture" />
-          />
-          <Route
-            path="/products/laptops"
-            element=<Products productCategory="Laptops" />
-          />
-          <Route
-            path="/products/:product-title"
-            element=<SingleProduct />
-          ></Route>
-          <Route
-            path="/search/:searchTerm"
-            element=<Products isSearchResults={true} />
-          />
-          <Route path="/admin" element=<Home /> />
-          <Route path="/cart" element=<Home /> />
-        </Routes>
-      </ProductContextProvider>
+      <Routes>
+        <Route path="/" element=<Home /> />
+        <Route
+          path="/products/mobiles"
+          element=<Products productCategory="Mobiles" />
+        />
+        <Route
+          path="/products/furniture"
+          element=<Products productCategory="Furniture" />
+        />
+        <Route
+          path="/products/laptops"
+          element=<Products productCategory="Laptops" />
+        />
+        <Route path="/search/:searchTerm" element=<Products isSearchResults={true} /> />
+        <Route path="/admin" element=<Home /> />
+        <Route path="/cart" element=<Home /> />
+      </Routes>
     </BrowserRouter>
   );
 };
--- frontend/src/components/ProductCards.tsx
+++ frontend/src/components/ProductCards.tsx
@@ -1,51 +1,37 @@
-import { useContext } from 'react';
-import ProductContext from '#src/context/ProductContext.tsx';
-
-import { Product } from '#src/types/types';
+import { Product } from '#src/types/types'
 
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 interface ProductCardsProps {
-  products: Product[];
+  products: Product[]
 }
 
-const ProductCards = (props: ProductCardsProps) => {
-  const context = useContext(ProductContext);
 
+const ProductCards = (props: ProductCardsProps) => {
   const productsToCards = props.products.map((product) => {
     let imageUrl = '';
     if (product.imgs !== null && product.imgs !== undefined) {
       imageUrl = `http://localhost:3003${product.imgs[0]}`;
     }
-
-    const productTitleToUri = product.title.split(' ').join('-');
-
-    const handleCardClick = () => {
-      if (context) {
-        context.productDispatch({ type: 'UPDATE', payload: 'test' });
-      }
-    };
-    
+  
     return (
-      <a href={`/products/${productTitleToUri}`} key={product.id}>
-        <Card key={product.id} onClick={handleCardClick}>
-          <Card.Img
-            variant="top"
-            src={imageUrl}
-            style={{ height: 200, width: 'auto' }}
-          />
-          <Card.Body>
-            <Card.Title>{product.title}</Card.Title>
-            <Card.Subtitle>{product.price}</Card.Subtitle>
-            <Card.Text>{product.specs[0].substring(0, 20)}</Card.Text>
-          </Card.Body>
-        </Card>
-      </a>
+      <Card key={product.id}>
+        <Card.Img
+          variant="top"
+          src={imageUrl}
+          style={{ height: 200, width: 'auto' }}
+        />
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Subtitle>{product.price}</Card.Subtitle>
+          <Card.Text>{product.specs[0].substring(0, 20)}</Card.Text>
+        </Card.Body>
+      </Card>
     );
   });
-
+  
   return (
     <Row lg={6}>
       {productsToCards.map((card) => (
@@ -53,6 +39,7 @@ const ProductCards = (props: ProductCardsProps) => {
       ))}
     </Row>
   );
-};
+}
 
-export default ProductCards;
+export default ProductCards
+
