e41cd99fa4d74df9f5f82671055bdfd1cd7799e4
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-11-11 16:02:42 +0200

    implementing Formik

--- frontend/src/components/ReviewForm.tsx
+++ frontend/src/components/ReviewForm.tsx
@@ -1,88 +1,103 @@
-import { useState } from 'react';
-
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import * as formik from 'formik';
+import * as yup from 'yup';
+
+interface ReviewFormValues {
+  title: string;
+  rating: number;
+  content: string;
+}
 
 const ReviewForm = ({ productId }: { productId: string | undefined }) => {
-  const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
-  const [rating, setRating] = useState<number>(1);
-  const [validated, setValidated] = useState<boolean>(false);
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+  const handleSubmit = () => {
+    // event.preventDefault();
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
 
     // setValidated(true);
     console.log('validated');
     // await reviewsService.getAllForProduct(productId);
   };
 
+  const { Formik } = formik;
+
+  const formSchema = yup.object().shape({
+    title: yup.string().required(),
+    rating: yup.number().required().oneOf([1, 2, 3, 4, 5]),
+    content: yup.string().required()
+  });
+
   return (
     <>
       <h2>Leave a review</h2>
-      <Form
-        noValidate
+      <Formik<ReviewFormValues>
+        validationSchema={formSchema}
         onSubmit={handleSubmit}
-        validated={validated}
-        className="mb-5"
+        initialValues={{
+          title: '',
+          rating: 1,
+          content: ''
+        }}
       >
-        <Form.Group controlId="validationReviewTitle" className="mb-3">
-          <Form.Label>
-            <b>Title</b>
-          </Form.Label>
-          <InputGroup hasValidation>
-            <Form.Control
-              required
-              isInvalid
-              type="text"
-              placeholder="Enter a review title"
-              className="mb-3"
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-            ></Form.Control>
-            <Form.Control.Feedback type="invalid">
-              Enter a title
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-        <Form.Label>
-          <b>Rating</b>
-        </Form.Label>
-        <Form.Select
-          value={rating}
-          onChange={(event) => setRating(Number(event.target.value))}
-          className="mb-3"
-        >
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Form.Select>
-        <Form.Control
-          required
-          as="textarea"
-          rows={5}
-          placeholder="Write a review"
-          value={content}
-          onChange={(event) => setContent(event.target.value)}
-        ></Form.Control>
+        {({ handleSubmit, handleChange, values, touched, errors }) => (
+          <Form noValidate onSubmit={handleSubmit} className="mb-5">
+            <Form.Group controlId="validationReviewTitle" className="mb-3">
+              <Form.Label>
+                <b>Title</b>
+              </Form.Label>
+              <Form.Control
+                type="text"
+                name="title"
+                value={values.title}
+                // placeholder="Enter a review title"
+                className="mb-3"
+                onChange={handleChange}
+                isValid={touched.title && !errors.title}
+              ></Form.Control>
+              <Form.Control.Feedback>Enter a title</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>
+                <b>Rating</b>
+              </Form.Label>
+              <Form.Select
+                value={values.rating}
+                onChange={handleChange}
+                className="mb-3"
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                required
+                as="textarea"
+                rows={5}
+                placeholder="Write a review"
+                value={values.content}
+                onChange={handleChange}
+              ></Form.Control>
+            </Form.Group>
 
-        <Button
-          type="submit"
-          size="lg"
-          // onClick={void handleReviewSend(productId)}
-          className="custom-button"
-        >
-          Send
-        </Button>
-      </Form>
+            <Button
+              type="submit"
+              size="lg"
+              // onClick={void handleReviewSend(productId)}
+              className="custom-button"
+            >
+              Send
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
