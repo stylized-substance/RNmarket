a79b91923ee1be9e64e676b8428316c87d50834f
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-01-20 09:58:53 +0200

    removed redundant import

--- frontend/src/components/Navbar/CartMenu.tsx
+++ frontend/src/components/Navbar/CartMenu.tsx
@@ -2,7 +2,6 @@ import { useCart } from '#src/context/CartContext.tsx';
 import { useNavigate } from 'react-router-dom';
 
 import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
