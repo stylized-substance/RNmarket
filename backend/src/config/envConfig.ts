96d572f283baab04e64fed0b76b4240680b76983
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-03-28 13:12:45 +0200

    changes

--- backend/src/config/envConfig.ts
+++ backend/src/config/envConfig.ts
@@ -3,7 +3,7 @@ import logger from '#src/utils/logger';
 import dotenv from 'dotenv';
 
 interface EnvVariables {
-  PORT: number | undefined;
+  PORT: number;
   DATABASE_URL: string;
   JWTACCESSTOKENEXPIRATION: number;
   JWTREFRESHTOKENEXPIRATION: number;
@@ -59,7 +59,7 @@ const allVariablesDefined: boolean = Object.values(envVariables).every(
 const reassign = (): EnvVariables => {
   return {
     PORT: process.env.PORT ? parseNumber(Number(process.env.PORT)) : 3003,
-    DATABASE_URL: process.env.DATABASE_URL ? parseString(setDatabaseUrl()) : '',
+    DATABASE_URL: parseString(setDatabaseUrl()),
     JWTACCESSTOKENEXPIRATION: process.env.JWTACCESSTOKENEXPIRATION
       ? parseNumber(Number(process.env.JWTACCESSTOKENEXPIRATION))
       : 3600,
@@ -85,10 +85,7 @@ if (!allVariablesDefined) {
 
 for (const key of Object.keys(envVariables) as (keyof EnvVariables)[]) {
   if (!variableDefined(envVariables[key])) {
-    logger(
-      `Environment variable ${key} missing, exiting. Are you missing an .env file at project root or did you forget to set some variable?`
-    );
-    process.exit();
+    throw new Error(`Environment variable ${key} missing, exiting. Are you missing an .env file at project root or did you forget to set some variable?`)
   }
 }
 
